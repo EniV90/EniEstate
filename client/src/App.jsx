0f45a9b544279702499c07682cebd2efa38c77cb
Victor <user@Victors-MacBook-Pro.local>
2024-05-29 10:24:00 +0100

    inital commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
