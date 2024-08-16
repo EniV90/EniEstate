272d2732bf3a6bc637c31283b1b10eae783303e9
Victor <user@Victors-MacBook-Pro.local>
2024-08-16 05:22:51 +0100

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,40 @@
-
+/* eslint-disable react/no-unescaped-entities */
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Eni Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        At Eni Estate, we believe that your home is more than just a place to
+        live, it's a reflection of your lifestyle, aspirations, and dreams. With
+        a deep commitment to excellence, we specialize in offering an exclusive
+        selection of luxurious properties that cater to discerning tastes.
+        Whether you're seeking a tranquil retreat, a bustling urban residence,
+        or a spacious family home, our curated listings are designed to meet
+        your unique needs and preferences.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our team of experienced real estate professionals is dedicated to
+        providing personalized service that goes beyond expectations. We
+        understand that finding the perfect home is a significant journey, and
+        we are here to guide you every step of the way. From initial
+        consultation to closing the deal, Eni Estate ensures a seamless
+        experience, helping you make informed decisions with confidence. Our
+        commitment to integrity, transparency, and customer satisfaction is at
+        the heart of everything we do.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Eni Estate is more than just a real estate agency, it's a partner in
+        your journey to finding the home that best suits your lifestyle. We
+        pride ourselves on our deep understanding of the market, our network of
+        industry connections, and our unwavering dedication to our clients. Let
+        us help you discover the place you'll be proud to call home. Welcome to
+        Eni Estate, where your dream home becomes a reality.
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
